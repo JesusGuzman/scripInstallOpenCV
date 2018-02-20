4aa287dab2977ee57f338682232730092b0bc033
JesusGuzman <jessguzman044@gmail.com>
2018-02-19 19:13:59 -0600

    hello dummy

---
+++ installOpenCV.sh
@@ -0,0 +1,9 @@
+#En scrip instalamos OpenCV con un ambiente en anaconda
+#con python2.7 para tensorflow(kerasbb)
+echo 'Autodestrucción en... '
+sudo yum install opencv opencv-python opencv-devel.i686
+pip install opencv-python
+source ~/.bashrc
+source ~/.bash_profile
+
+echo 'Autodestruccion completada'
